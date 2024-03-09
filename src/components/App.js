e5b686397bb270f2a7996bfdd7937b4ad60307eb
Jose <37120821+zerta92@users.noreply.github.com>
2024-03-09 11:48:05 +0000

    identicon and navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from "react";
+import Web3 from "web3";
 import logo from "../logo.png";
 import "./App.css";
+import Navbar from "./Navbar";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { account: "" };
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You Should consider trying MetaMask!"
+      );
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+    this.setState({ account: accounts[0] });
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Fuckboy University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
