48d11c415ccd7dc9339b0f869a9a09b7c69b2ecb
Jose <37120821+zerta92@users.noreply.github.com>
2024-10-10 16:23:56 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -35,13 +35,11 @@ function App() {
     };
 
     if (window.ethereum) {
-      if (process.env.NODE_ENV === "production") {
+      if (false && process.env.NODE_ENV === "production") {
         window.web3 = new Web3(
           new Web3.providers.HttpProvider(
             `https://sepolia.infura.io/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}`
-          ),
-          null,
-          options
+          )
         );
       } else {
         window.web3 = new Web3(window.ethereum, null, options);
@@ -49,13 +47,11 @@ function App() {
 
       await window.ethereum.enable();
     } else if (window.web3) {
-      if (process.env.NODE_ENV === "production") {
+      if (false && process.env.NODE_ENV === "production") {
         window.web3 = new Web3(
           new Web3.providers.HttpProvider(
             `https://sepolia.infura.io/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}`
-          ),
-          null,
-          options
+          )
         );
       } else {
         window.web3 = new Web3(window.web3.currentProvider, null, options);
