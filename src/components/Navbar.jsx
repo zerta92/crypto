6a69340f7bb055b2ab223db94950bfeaae946b36
Jose <37120821+zerta92@users.noreply.github.com>
2024-10-10 08:37:04 +0100

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import { cacheData } from "./utils.js";
 
 function Navbar({ account, handleModalOpen, handleModalClose }) {
   const { currency, symbol, rate } = useGlobal();
-
   const { ethRate, btcRate } = useAlphavantage();
   const dispatchGlobal = useDispatchGlobal();
 
@@ -20,10 +19,10 @@ function Navbar({ account, handleModalOpen, handleModalClose }) {
   };
 
   return (
-    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <div className="container-fluid d-flex justify-content-between align-items-center">
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark p-0 shadow">
+      <div className="container-fluid">
         <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          className="navbar-brand"
           href="/"
           target="_self"
           rel="noopener noreferrer"
@@ -31,82 +30,93 @@ function Navbar({ account, handleModalOpen, handleModalClose }) {
           Summary
         </a>
 
-        <div className="d-flex align-items-center">
-          <span className="navbar-brand mr-3">
-            ETH: {symbol}
-            {(ethRate * rate).toFixed(2)}
-          </span>
-          <span className="navbar-brand mr-3">
-            BTC: {symbol}
-            {(btcRate * rate).toFixed(2)}
-          </span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <a
-            className="nav-item nav-link"
-            href="/transactions"
-            target="_self"
-            rel="noopener noreferrer"
-          >
-            Transactions
-          </a>
+        <div className="collapse navbar-collapse" id="navbarContent">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="/transactions" target="_self">
+                Transactions
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/posts" target="_self">
+                Notes
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="/"
+                onClick={(event) => {
+                  event.preventDefault();
+                  handleModalOpen();
+                }}
+              >
+                Log
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="/"
+                onClick={(event) => {
+                  event.preventDefault();
+                  dispatchGlobal({
+                    type: "setCurrency",
+                    payload: toggleCurrency(currency),
+                  });
+                }}
+              >
+                {currency}
+              </a>
+            </li>
+          </ul>
 
-          <a
-            className="nav-item nav-link"
-            href="/posts"
-            target="_self"
-            rel="noopener noreferrer"
-          >
-            Notes
-          </a>
-
-          <a
-            href="/"
-            className="nav-item nav-link"
-            onClick={(event) => {
-              event.preventDefault();
-              handleModalOpen();
-            }}
-          >
-            Log
-          </a>
-
-          <a
-            className="nav-item nav-link"
-            href="/"
-            target="_self"
-            onClick={(event) => {
-              event.preventDefault();
-              dispatchGlobal({
-                type: "setCurrency",
-                payload: toggleCurrency(currency),
-              });
-            }}
-          >
-            {currency}
-          </a>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item mr-3">
+              <span className="navbar-text">
+                ETH: {symbol}
+                {(ethRate * rate).toFixed(2)}
+              </span>
+            </li>
+            <li className="nav-item mr-3">
+              <span className="navbar-text">
+                BTC: {symbol}
+                {(btcRate * rate).toFixed(2)}
+              </span>
+            </li>
+            <li className="nav-item text-nowrap">
+              <small className="text-secondary">
+                <small id="account">{account}</small>
+                {account ? (
+                  <img
+                    alt=""
+                    className="ml-2"
+                    height="30"
+                    width="30"
+                    src={`data:image/png;base64,${new Identicon(
+                      account,
+                      30
+                    ).toString()}`}
+                  />
+                ) : (
+                  <span></span>
+                )}
+              </small>
+            </li>
+          </ul>
         </div>
-
-        <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small className="text-secondary">
-              <small id="account">{account}</small>
-              {account ? (
-                <img
-                  alt=""
-                  className="ml-2"
-                  height="30"
-                  width="30"
-                  src={`data:image/png;base64,${new Identicon(
-                    account,
-                    30
-                  ).toString()}`}
-                />
-              ) : (
-                <span></span>
-              )}
-            </small>
-          </li>
-        </ul>
       </div>
     </nav>
   );
