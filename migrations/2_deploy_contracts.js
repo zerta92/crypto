08652e722d2c594f1d190446f98442b87540121b
Jose <37120821+zerta92@users.noreply.github.com>
2024-10-12 12:02:13 +0100

    fix truffle migration
    switch from infura to alchemy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 const SocialNetwork = artifacts.require("SocialNetwork");
 const CryptoTransactions = artifacts.require("CryptoTransactions");
 
-module.exports = function(deployer) {
-  deployer.deploy(SocialNetwork);
-  deployer.deploy(CryptoTransactions);
+module.exports = async function (deployer) {
+  await deployer.deploy(SocialNetwork);
+  await deployer.deploy(CryptoTransactions);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -14,20 +14,20 @@ module.exports = {
       provider: () =>
         new HDWalletProvider(
           process.env.REACT_APP_DEPLOYER_PRIVATE_KEY,
-          `https://sepolia.infura.io/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}`
+          `https://eth-sepolia.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_PROJECT_ID}`
         ),
       network_id: 11155111, // Sepolia network ID
       gas: 5500000, // Gas limit (can adjust based on contract)
       confirmations: 2, // Wait for 2 confirmations
       skipDryRun: true, // Skip dry run before migrations
-      networkCheckTimeout: 10000,
-      timeoutBlocks: 200,
+      networkCheckTimeout: 1000000,
+      skipDryRun: true, // Skip dry run before migrations
     },
     mainnet: {
       provider: () =>
         new HDWalletProvider(
           process.env.REACT_APP_DEPLOYER_PRIVATE_KEY,
-          `https://mainnetxxx.infura.io/v3/${process.env.REACT_APP_INFURA_PROJECT_ID}` //added xxx for safety
+          `https://eth.mainnetxxx.g.alchemy.io/v3/${process.env.REACT_APP_ALCHEMY_PROJECT_ID}` //added xxx for safety
         ),
       network_id: 1, // Mainnet network ID
       gas: 5500000, // Adjust gas limit as necessary
